5b16b15b767955648f20dece9717beb978c7b521
Cathy Cheung <cathy.cheung@anu.edu.au>
2021-10-12 22:57:08 +0800

    parse from Json (logcat)

--- app/src/main/java/anu/softwaredev/socialmediacat/JsonTest.java
+++ app/src/main/java/anu/softwaredev/socialmediacat/JsonTest.java
@@ -3,27 +3,67 @@ package anu.softwaredev.socialmediacat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 public class JsonTest extends AppCompatActivity {
 
     TextView TV1;
     TextView TV2;
+    TextView TV3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_json_test);
+
+    }
+
+    // Trivial Test
+    public static User createUser() {
+
+        // Trash data
+        String uId = "1";
+        String userName = "user1";
+        String emailAddress = "user1@a.com";
+        String proPicLink = "https://google.com";
+        List<String> followings = new ArrayList<>(Arrays.asList("2", "3"));
+
+        return new User(uId, userName, emailAddress, proPicLink, followings);
+
+    }
+
+
+    /** Logcat test */
+    public static void logUsersFromJson(String jsonFileString) {
+        Log.i("data", jsonFileString);
+
+        Gson gson = new Gson();
+        Type listUserType = new TypeToken<List<User>>() { }.getType();
+        List<User> users = gson.fromJson(jsonFileString, listUserType);
+        for (int i=0; i < users.size(); i++) {
+            Log.i("data", "> Item " + i + "\n" + users.get(i));
+        }
 
     }
 
