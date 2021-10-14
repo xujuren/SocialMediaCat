36485fbdc1e2ad8db336397456bad2d6eecce49d
Cathy Cheung <cathy.cheung@anu.edu.au>
2021-10-14 08:58:35 +0800

    Add Location (CreatePost): Start Setting Up (Issue: listener slower than create post??), (changed location to button)

--- app/src/main/java/anu/softwaredev/socialmediacat/CreatePost.java
+++ app/src/main/java/anu/softwaredev/socialmediacat/CreatePost.java
@@ -1,12 +1,30 @@
 package anu.softwaredev.socialmediacat;
-import com.google.firebase.auth.FirebaseAuth;
-import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
+
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseUser;
@@ -14,6 +32,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import anu.softwaredev.socialmediacat.Classes.Post;
@@ -21,14 +40,18 @@ import anu.softwaredev.socialmediacat.Classes.Post;
 public class CreatePost extends AppCompatActivity {
     private FirebaseUser user;
     private DatabaseReference dbRef;
-    TextInputLayout contentLayout ;
-    TextInputLayout categoryLayout ;
-    TextInputLayout locLayout;
-    TextInputLayout photoURLLayout;
-    EditText contentEdit ;
-    EditText categoryEdit ;
-    EditText locEdit;
-    EditText photoURLEdit;
+    private TextInputLayout contentLayout;
+    private TextInputLayout categoryLayout;
+    private ToggleButton shareLocOption;
+    private TextInputLayout photoURLLayout;
+    private EditText contentEdit;
+    private EditText categoryEdit;
+    private EditText photoURLEdit;
+    private LocationManager locManager;
+    private LocationListener locListener;
+    private String lat;
+    private String lng;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,16 +60,16 @@ public class CreatePost extends AppCompatActivity {
 
         initView();
 
+
     }
 
     public void initView() {
         contentLayout = (TextInputLayout) findViewById(R.id.content_createpost);
         categoryLayout = (TextInputLayout) findViewById(R.id.category_createpost);
-        locLayout = (TextInputLayout) findViewById(R.id.loc_createpost);
+        shareLocOption = (ToggleButton) findViewById(R.id.bt_shareLoc);
         photoURLLayout = (TextInputLayout) findViewById(R.id.photo_createpost);
         contentEdit = (EditText) findViewById(R.id.content_createpost_et);
         categoryEdit = (EditText) findViewById(R.id.category_createpost_et);
-        locEdit = (EditText) findViewById(R.id.loc_createpost_et);
         photoURLEdit = (EditText) findViewById(R.id.photo_createpost_et);
     }
 
@@ -59,9 +82,36 @@ public class CreatePost extends AppCompatActivity {
             // Get Input
             String content = contentEdit.getText().toString();
             String category = categoryEdit.getText().toString();
-            String locShare = locEdit.getText().toString();
+            Boolean shareLocBool = shareLocOption.getText().toString().equals("Location will be shared!");
             String photoURL = photoURLEdit.getText().toString();
+
+
+            // TODO
+            /** shd be > UserActivityDAO */
+
+            if (shareLocBool) {
+                getLatLong();   // locationListener
+                getLoc();
+
+
+                Toast.makeText(CreatePost.this, "ll: " + lat + "," + lng, Toast.LENGTH_LONG).show();
+                content = content + "["+lat+", "+lng+"]";
+            }
+
+            // URL
+            Boolean photoURLValid = false;
+            if (!photoURL.equals("")) {
+                try {
+                    (new java.net.URL(photoURL)).openStream().close();
+                    photoURLValid = true;
+                } catch (Exception ex) {
+                }
+            }
+            Toast.makeText(CreatePost.this, "PhotoValid? - " + photoURLValid.toString(), Toast.LENGTH_SHORT);
+
+
             String postId = "P101";                                     //TODO - unique, new
+
 
             if (!TextUtils.isEmpty(content) && !TextUtils.isEmpty(category)) {
 
@@ -78,17 +128,79 @@ public class CreatePost extends AppCompatActivity {
 
                 dbRef.updateChildren(childUpdates);
 
+                Toast.makeText(CreatePost.this, "Post Created!", Toast.LENGTH_LONG).show();
+                finish();
+
             } else {
                 Toast.makeText(CreatePost.this, "You did not enter the post Category and/or Content!", Toast.LENGTH_LONG).show();
                 finish();
             }
 
         } else {
-                Toast.makeText(CreatePost.this, "An error occur. Sorry for the inconveniences", Toast.LENGTH_LONG).show();
-                finish();   // UNEXPECTED Branch
+            Toast.makeText(CreatePost.this, "An error occur. Sorry for the inconveniences", Toast.LENGTH_LONG).show();
+            finish();   // UNEXPECTED Branch
+        }
+
+    }
+
+    public void getLoc() {
+        locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // check Permissions
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    || checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    || checkSelfPermission(Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}, 10);
+            }
+        }
+
+        locManager.requestLocationUpdates("gps", 10000, 1000, locListener);
+
+        // Toast.makeText(CreatePost.this, "ll: "+latLong[0]+","+latLong[1], Toast.LENGTH_LONG).show();
+    }
+
+    /** location listener */
+    private void getLatLong() {
+        locListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+
+                lat = String.format("%.2f", location.getLatitude());
+                lng = String.format("%.2f", location.getLongitude());
+
+                // TODO - super slow?? persistent?
+                // Toast.makeText(CreatePost.this, "in getLL(): "+ lat+","+lng, Toast.LENGTH_LONG).show();
             }
 
-        }
+            @Override
+            public void onLocationChanged(@NonNull List<Location> locations) {
+
+            }
+
+            @Override
+            public void onFlushComplete(int requestCode) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(@NonNull String provider) {
+                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+            }
+
+            @Override
+            public void onProviderDisabled(@NonNull String provider) {
+            }
+        };
+
+    }
+
+
 
 
 
