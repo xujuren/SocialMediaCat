52513d34286300cc24cbf2d68a2c513b2724fe49
kev <kevinwjh520@gmail.com>
2021-10-16 19:38:28 +1100

    ProfileFragment cleanup done

--- app/src/main/java/anu/softwaredev/socialmediacat/ProfileFragment.java
+++ app/src/main/java/anu/softwaredev/socialmediacat/ProfileFragment.java
@@ -29,6 +29,7 @@ public class ProfileFragment extends Fragment {
         // Required empty public constructor
     }
 
+
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
