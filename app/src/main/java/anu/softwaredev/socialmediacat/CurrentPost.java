89a851209d201870c8d210c50607884e155c7e25
Cathy Cheung <cathy.cheung@anu.edu.au>
2021-10-20 18:01:12 +0800

    revised layout draft

--- app/src/main/java/anu/softwaredev/socialmediacat/CurrentPost.java
+++ app/src/main/java/anu/softwaredev/socialmediacat/CurrentPost.java
@@ -15,7 +15,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import Tree.Global_Data;
 import anu.softwaredev.socialmediacat.Classes.Post;
-import anu.softwaredev.socialmediacat.dao.UserActivity.UserActivity;
 import anu.softwaredev.socialmediacat.dao.UserActivity.UserActivityDao;
 //like button library
 import com.like.LikeButton;
@@ -40,29 +39,58 @@ public class CurrentPost extends AppCompatActivity {
         int photoId = intent.getIntExtra("photoId",0);
         int likeCount = intent.getIntExtra("likeCount",0);
         currentPost = new Post(uid, tag, postId, content, photoId, likeCount);
-        CharSequence likes = "Total Like : " + currentPost.getLikeCount();
-
 
         ImageView image = (ImageView) findViewById(R.id.imageView);
-        Glide.with(getApplicationContext()).load("https://picsum.photos/id/" + photoId + "/300/200").apply(new RequestOptions())
+        Glide.with(getApplicationContext()).load("https://picsum.photos/id/" + photoId + "/400/300").apply(new RequestOptions())
                 .diskCacheStrategy(DiskCacheStrategy.NONE)
                 .skipMemoryCache(true)
                 .into(image);
 
 
-        TextView contentv = (TextView) findViewById(R.id.titleTextView);
-        contentv.setText((CharSequence)currentPost.getContent());
-        contentv.setTextSize(32f);
-        contentv.setTypeface(Typeface.DEFAULT_BOLD);
-        TextView like = (TextView) findViewById(R.id.ContentTextView);
-        like.setText(likes);
-        like.setTextSize(32f);
-        like.setTypeface(Typeface.DEFAULT_BOLD);
-        //post id show
-        TextView postID = (TextView) findViewById(R.id.PostIdtextView);
+        // post id show
+        TextView postID = (TextView) findViewById(R.id.postIdTextView);
         postID.setText((CharSequence)currentPost.getPostId());
-        postID.setTextSize(16f);
+        postID.setTextSize(15f);
         postID.setTypeface(Typeface.DEFAULT_BOLD);
+
+        // user id show
+        TextView uId = (TextView) findViewById(R.id.userIdTextView);
+        uId.setText("@"+(CharSequence)currentPost.getUId());
+        uId.setTextSize(15f);
+        uId.setTypeface(Typeface.DEFAULT_BOLD);
+
+        TextView like = (TextView) findViewById(R.id.likeTextView);
+        CharSequence likes = currentPost.getLikeCount() + "likes";
+        like.setText(likes);
+        like.setTextSize(15f);
+        like.setTypeface(Typeface.DEFAULT_BOLD);
+
+        // tag show (if any)
+        TextView tagTv = (TextView) findViewById(R.id.tagTextView);
+        tagTv.setText((CharSequence)currentPost.getTag());
+        tagTv.setTextSize(20f);
+        tagTv.setTypeface(Typeface.DEFAULT_BOLD);
+
+        TextView contentv = (TextView) findViewById(R.id.contentTextView);
+        contentv.setText((CharSequence)currentPost.getContent());
+        contentv.setTextSize(20f);
+        postID.setTypeface(Typeface.DEFAULT);
+
+
+        // Now get a handle to any View contained
+        // within the main layout you are using
+        View someView = findViewById(R.id.randomViewInMainLayout);
+
+        // Find the root view
+        View root = someView.getRootView();
+
+        // Set the color
+        root.setBackgroundColor(getResources().getColor(android.R.color.red));
+
+
+
+
+
 
 //        Button likeBt = (Button) findViewById(R.id.LikeButton);
 //        likeBt.setOnClickListener(new View.OnClickListener() {
@@ -116,14 +144,11 @@ public class CurrentPost extends AppCompatActivity {
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 boolean likeResult = Global_Data.getInstance().likePost(currentPost);
                 if (likeResult){
-                    long likec = currentPost.getLikeCount();
-                    CharSequence dolike = "Total Like: " + likec;
-                    like.setText(dolike);
-                    UserActivityDao.getInstance().likePost(user.getUid(), currentPost.getPostId());
+                    CharSequence likes = currentPost.getLikeCount() + "likes";
+                    like.setText(likes);
+                    UserActivityDao.getInstance().dislikePost(user.getUid(), currentPost.getPostId());
                     //showing simple Toast when unLiked
                     Toast.makeText( CurrentPost.this, " Post UnLiked  : )", Toast.LENGTH_SHORT ).show(  );}
-
-
             }
         } );
 
