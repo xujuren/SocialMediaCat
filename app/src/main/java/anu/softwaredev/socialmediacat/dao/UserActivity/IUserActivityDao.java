3812aee0f8da811641a752a3ea6a32ecb105d422
Cathy Cheung <cathy.cheung@anu.edu.au>
2021-10-22 10:18:05 +0800

    DAO and iDAO documented

--- app/src/main/java/anu/softwaredev/socialmediacat/dao/UserActivity/IUserActivityDao.java
+++ app/src/main/java/anu/softwaredev/socialmediacat/dao/UserActivity/IUserActivityDao.java
@@ -1,24 +1,31 @@
 package anu.softwaredev.socialmediacat.dao.UserActivity;
 
+import android.content.Context;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseUser;
+
 import java.util.List;
 
 import anu.softwaredev.socialmediacat.Classes.Post;
 
+/**
+ * Defines the data access and persistence tasks in relation to UserActivity
+ */
 public interface IUserActivityDao {
 
-    /** 5 Methods */
+    void createPost(String uId, String tags, String content, int photoId);
 
-    public void createPost(String uId, String tags, String content, int photoId);
+    void likePost(String postId);
 
-    public void likePost(String postId);
+    void unlikePost(String postId);
 
-    public void deletePost(String postId);
+    void deletePost(String postId);
 
-    public List<Post> findAllPosts();       // change to get Posts
+    void findUserProfile(String userId, TextView uIdTv, TextView captionTv);
 
-    public String getFilePath();
+    void findUserProfile(FirebaseUser user, TextInputLayout userNameLayout, TextInputLayout interestsLayout, TextInputLayout captionLayout);
 
-
-    public void deleteAll();
-
+    void updateProfile(Context ctx, FirebaseUser user, String newName, String newInterests, String newCaption);
 }
